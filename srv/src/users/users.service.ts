401c189787e558f882d47c4a31d2a866fb129788
Alexey <suxovarov.alexey@gmail.com>
2024-04-17 16:39:13 +0300

    findAll with pagination

--- srv/src/users/users.service.ts
+++ srv/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
 import { Repository } from 'typeorm';
+import { createPaginationLinks } from './users.util';
 
 @Injectable()
 export class UserService {
@@ -13,7 +14,22 @@ export class UserService {
   ) {}
 
   // get list of all users
-  async findAll(): Promise<UsersEntity[]> {
-    return await this.usersRepo.find();
+  async findAll(page = 1, limit = 20): Promise<{ data: UsersEntity[]; meta: object }> {
+    const [data, total] = await this.usersRepo.findAndCount({
+      skip: page > 0 ? (page - 1) * limit : 0,
+      take: limit,
+    });
+
+    const meta = {
+      total,
+      page,
+      limit,
+      links: createPaginationLinks(page, limit, total),
+    };
+
+    return {
+      data,
+      meta,
+    };
   }
 }
